6c4c3ffd7371f5244daaf0db7b5636d1890dc3bb
Dreamerxgod <91774479+Dreamerxgod@users.noreply.github.com>
2024-06-02 21:28:04 +0300

    sort

--- untitled5/mainwindow.h
+++ untitled5/mainwindow.h
@@ -6,8 +6,10 @@
 #include <QPushButton>
 #include <QToolBar>
 #include <QString>
+#include <QHeaderView>
 #include <map>
-#include <vector>
+#include "customtablewidgetitem.h"
+
 
 namespace Ui {
 class MainWindow;
@@ -20,35 +22,6 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
-    //"Player Name", "Salary", "Position", "Age", "Team", "Games Played", "Games Started","Min/Game", "FG", "FGA", "3P", "3PA",
-    //"2P", "2PA", "ORB", "DRB", "AST", "STL", "BLK", "PF", "PTS", "Total minutes", "PER"
-    struct player
-    {
-        int sal;
-        QString pos;
-        int age;
-        QString team;
-        int gp;
-        int gs;
-        double mg;
-        double fg;
-        double fga;
-        double threeP;
-        double threePA;
-        double twoP;
-        double twoPA;
-        double orb;
-        double drb;
-        double ast;
-        double stl;
-        double blk;
-        double pf;
-        double pts;
-        int tm;
-        double per;
-    };
-    std::map <QString, std::vector<player>> players;
 
 private slots:
     void addPlayer();
@@ -67,10 +40,36 @@ private:
 
     void readCSV(const QString &filename);
     void writeCSV(const QString &filename);
+    void sortData(int column);  // Add this line
 };
 
-
-
-
+//        "Player Name", "Salary", "Position", "Age", "Team", "Games Played", "Games Started","Min/Game", "FG", "FGA", "3P", "3PA",
+//"2P", "2PA", "ORB", "DRB", "AST", "STL", "BLK", "PF", "PTS", "Total minutes", "PER",
+struct player
+{
+    QString name;
+    int sal;
+    QString pos;
+    int age;
+    QString team;
+    int gp;
+    int gs;
+    double mg;
+    double fg;
+    double fga;
+    double threeP;
+    double threePA;
+    double twoP;
+    double twoPA;
+    double orb;
+    double drb;
+    double ast;
+    double stl;
+    double blk;
+    double pf;
+    double pts;
+    int tm;
+    double per;
+};
 
 #endif // MAINWINDOW_H
