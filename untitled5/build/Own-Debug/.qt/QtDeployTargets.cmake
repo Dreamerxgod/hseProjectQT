490357a2d03dd5b3fa0105bc7e039ed71bef6347
Bot1noke <alexsys006.ivanov@gmail.com>
2024-06-02 12:36:07 +0300

    Added dataset

---
+++ untitled5/build/Own-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,1 @@
+set(__QT_DEPLOY_TARGET_untitled5_FILE /Users/bot1nok/Desktop/hseProjectQT/untitled5/build/Own-Debug/untitled5.app/Contents/MacOS/untitled5)
---
+++ untitled5/build/Own-Debug/CMakeFiles/untitled5_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled5_autogen"
+  "untitled5_autogen/include/ui_dialog.h"
+  "untitled5_autogen/include/ui_mainwindow.h"
+  "untitled5_autogen/mocs_compilation.cpp"
+  "untitled5_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/untitled5_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
